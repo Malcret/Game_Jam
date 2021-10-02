ae642447bad7dd1a51c2e28579488aae41df9b2d
lxtrem <33664866+lxtrem@users.noreply.github.com>
2021-10-03 01:08:54 +0200

    Added: loadSettings function

--- include/settings.hpp
+++ include/settings.hpp
@@ -1,10 +1,14 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
 
-struct {
+#include <yaml-cpp/yaml.h>
+
+typedef struct {
     unsigned int window_width;
     unsigned int window_height;
     unsigned int antialiasing;
 } settings;
+
+settings loadSettings();
 
 #endif // SETTINGS_HPP
