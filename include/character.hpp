85822e40941aadf364ced5e905cf9a555a4bfc53
Malcret <t.marcopoulos@outlook.fr>
2021-10-02 10:11:43 +0200

    update tree and add character shader

--- include/character.hpp
+++ include/character.hpp
@@ -123,12 +123,12 @@ public:
             float h = character.size.y * scale;
             // update VBO for each character
             float vertices[6][4] = {
-                { xpos, ypos + h, 0.0f, 0.0f },            
-                { xpos, ypos, 0.0f, 1.0f },
-                { xpos + w, ypos, 1.0f, 1.0f },
-                { xpos, ypos + h, 0.0f, 0.0f },
-                { xpos + w, ypos, 1.0f, 1.0f },
-                { xpos + w, ypos + h, 1.0f, 0.0f }           
+                {xpos, ypos + h, 0.0f, 0.0f},            
+                {xpos, ypos, 0.0f, 1.0f},
+                {xpos + w, ypos, 1.0f, 1.0f},
+                {xpos, ypos + h, 0.0f, 0.0f},
+                {xpos + w, ypos, 1.0f, 1.0f},
+                {xpos + w, ypos + h, 1.0f, 0.0f}           
             };
             // render glyph texture over quad
             glBindTexture(GL_TEXTURE_2D, character.textureID);
