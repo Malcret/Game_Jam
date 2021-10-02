056d5af9820f22777426233cf457d198a3a98b23
Malcret <t.marcopoulos@outlook.fr>
2021-10-02 08:07:27 +0200

    add lib files, include files and switch to Makefile instead of bash script

---
+++ Makefile
@@ -0,0 +1,55 @@
+OS = linux
+BUILD_MOD = debug
+CXX_VERSION = c++20
+PROG_NAME = prog
+
+INC_DIR = -I./include
+LIB_DIR = -L./lib
+
+ifeq ($(OS), linux)
+LIB_FILES = -lGL -lGLEW -lglfw3 -lglm_static -lfreetype -lX11 -lXxf86vm -lXrandr -pthread -lXi -ldl
+else ifeq ($(OS), win32)
+LIB_FILES =
+else
+@echo Wrong OS, use 'linux' or 'win32'
+endif
+
+ifeq ($(BUILD_MOD), debug)
+CXX_FLAGS = -std=$(CXX_VERSION) -g -Og -Wall
+else ifeq ($(BUILD_MOD), release)
+CXX_FLAGS = -std=$(CXX_VERSION) -O2 -DNDEBUG
+else
+@echo Wrong build mod, use 'debug' or 'release'
+endif
+
+SRC_FILES = $(wildcard src/*.cpp)
+OBJ_FILES = $(patsubst src/%.cpp, bin/$(OS)/$(BUILD_MOD)/obj/%.o, $(SRC_FILES))
+PROG = bin/$(OS)/$(BUILD_MOD)/$(PROG_NAME)
+
+bin/$(OS)/$(BUILD_MOD)/obj/%.o: src/%.cpp
+	mkdir -p $(dir $@)
+	g++ $(CXX_FLAGS) $(INC_DIR) -o $@ -c $<
+
+$(PROG): $(OBJ_FILES)
+	g++ $(LIB_DIR) -o $@ $^ $(LIB_FILES)
+
+.PHONY: build run clean mrproper help
+
+build: $(PROG)
+
+run:
+	./$(PROG)
+
+clean:
+	rm -Rf bin/$(OS)/$(BUILD_MOD)/*.o
+	rm -Rf $(PROG)
+
+mrproper:
+	rm -Rf bin/*
+
+help:
+	@echo "'build' to build your code"
+	@echo "'run' to run your program"
+	@echo "'clean' to remove all object files associate to your build mod"
+	@echo "'mrproper' to remove your 'build' directory"
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include "shader.hpp"
-#include "texture.hpp"
-
 // OpenGL context options
 #define ANTIALIASING 4
 #define OPENGL_MAJOR 4
@@ -20,6 +17,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 static void character_callback(GLFWwindow *window, unsigned int codepoint);
 static void cursor_position_callback(GLFWwindow *window, double xpos, double ypos);
 static void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+static void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 // Exit with error function
 static void exit_error(const char *msg, GLFWwindow *window, bool terminate_GLFW);
@@ -54,6 +52,7 @@ int main(int argc, const char **argv) {
     glfwSetCharCallback(window, character_callback);
     glfwSetCursorPosCallback(window, cursor_position_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Initialize GLEW
     glewExperimental = 1;
@@ -64,6 +63,10 @@ int main(int argc, const char **argv) {
     // Set capture modes
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GL_TRUE);
+
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
@@ -105,6 +108,12 @@ static void cursor_position_callback(GLFWwindow *window, double xpos, double ypo
 // Get mouse input
 static void mouse_button_callback(GLFWwindow *window, int button, int action, int mods) {
 
+}
+
+// Set window size
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    
+    glViewport(0, 0, width, height);
 }
 
 // Exit with error
