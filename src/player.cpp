b14b4ba6ebe3d86544a30ed095b853839665e577
lxtrem <33664866+lxtrem@users.noreply.github.com>
2021-10-03 01:07:32 +0200

    Update: Saving methods

--- src/player.cpp
+++ src/player.cpp
@@ -9,6 +9,34 @@ Player::~Player() {
 
 }
 
+void Player::initStats() {
+    m_madness = m_default_madness;
+    m_health = m_default_health;
+    set_pos(glm::vec2(0,0));
+}
+
+void Player::save() {
+    std::string nom(m_name);
+    YAML::Node saveFile = YAML::LoadFile("save.yml");
+    std::ofstream outFile("save.yml");
+    saveFile["playerName"] = nom;
+    saveFile["madness"] = m_madness;
+    saveFile["health"] = m_health;
+    saveFile["pos"]["x"] = m_pos.x;
+    saveFile["pos"]["y"] = m_pos.y;
+    outFile << saveFile;
+    outFile.close();
+}
+
+void Player::initFromSave() {
+    YAML::Node saveFile = YAML::LoadFile("save.yml");
+    m_madness = saveFile["madness"].as<unsigned int>();
+    m_health = saveFile["health"].as<unsigned int>();
+    m_pos.x = saveFile["pos"]["x"].as<int>();
+    m_pos.y = saveFile["pos"]["y"].as<int>();
+    m_name = saveFile["playerName"].as<std::string>().c_str();
+}
+
 void Player::set_pos(glm::vec2 pos) {
     m_pos = pos;
 }
